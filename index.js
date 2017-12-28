eaa9bb1a179fd7686bafabdaf5298ba00b76924b
yoku001 <yoku001@users.noreply.github.com>
2017-12-29 01:42:58 +0900

    パフォーマンスモニタ追加

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ class Neighbor {
     this.distance = Math.sqrt(this.nx * this.nx + this.ny * this.ny)
     this.weight = 1.0 - this.distance / RANGE
 
-    var temp = this.weight * this.weight * this.weight
+    let temp = this.weight * this.weight * this.weight
     p1.density += temp
     p2.density += temp
     temp = 1.0 / this.distance
@@ -62,16 +62,16 @@ class Neighbor {
   }
 
   calcForce () {
-    var p1 = this.p1
-    var p2 = this.p2
-    var pressureWeight = this.weight * (p1.pressure + p2.pressure) / (p1.density + p2.density) * PRESSURE
-    var viscosityWeight = this.weight / (p1.density + p2.density) * VISCOSITY
+    let p1 = this.p1
+    let p2 = this.p2
+    const pressureWeight = this.weight * (p1.pressure + p2.pressure) / (p1.density + p2.density) * PRESSURE
+    const viscosityWeight = this.weight / (p1.density + p2.density) * VISCOSITY
     p1.fx += this.nx * pressureWeight
     p1.fy += this.ny * pressureWeight
     p2.fx -= this.nx * pressureWeight
     p2.fy -= this.ny * pressureWeight
-    var rvx = p2.vx - p1.vx
-    var rvy = p2.vy - p1.vy
+    const rvx = p2.vx - p1.vx
+    const rvy = p2.vy - p1.vy
     p1.fx += rvx * viscosityWeight
     p1.fy += rvy * viscosityWeight
     p2.fx -= rvx * viscosityWeight
@@ -95,42 +95,53 @@ class Grid {
 }
 
 window.onload = (function () {
-  var mouseX
-  var mouseY
-  var RANGE2 = RANGE * RANGE
-  var DENSITY = 0.2
-  var NUM_GRIDS = 46
-  var INV_GRID_SIZE = 1.0 / (465.0 / NUM_GRIDS)
-  var canvas = document.getElementById('canvas')
-  var ctx = canvas.getContext('2d')
-  var particles = []
-  var numParticles = 0
-  var neighbors = []
-  var numNeighbors = 0
-  var count = 0
-  var press = false
-  var grids = []
+  let mouseX
+  let mouseY
+  const RANGE2 = RANGE * RANGE
+  const DENSITY = 0.2
+  const NUM_GRIDS = 46
+  const INV_GRID_SIZE = 1.0 / (465.0 / NUM_GRIDS)
+  const canvas = document.getElementById('canvas')
+
+  // Stats
+  const stats = new Stats()
+  stats.showPanel(0)
+  document.body.appendChild(stats.domElement)
+  const ctx = canvas.getContext('2d')
+  ctx.font = '20px Arial'
+  let particles = []
+  let numParticles = 0
+  let neighbors = []
+  let numNeighbors = 0
+  let count = 0
+  let press = false
+  let grids = []
 
   function frame (e) {
+    stats.begin()
+
     if (press) { pour() }
     move()
     ctx.clearRect(0, 0, 460, 460)
     draw()
-    // window.setTimeout(frame, 1000 / 60)
+
+    stats.end()
     window.requestAnimationFrame(frame)
   }
 
   function draw () {
-    for (var i = 0; i < numParticles; i++) {
-      var p = particles[i]
+    ctx.fillText(numParticles, 10, 20)
+
+    for (let i = 0; i < numParticles; i++) {
+      let p = particles[i]
       ctx.fillRect(p.x - 1.5, p.y - 1.5, 3, 3)
     }
   }
 
   function pour () {
     if (count % 3 === 0) {
-      for (var i = -4; i <= 4; i++) {
-        var p = new Particle(mouseX + i * 8, mouseY)
+      for (let i = -7; i <= 8; i++) {
+        let p = new Particle(mouseX + i * 8, mouseY)
         p.vy = 3
         particles[numParticles++] = p
       }
@@ -145,19 +156,17 @@ window.onload = (function () {
     findNeighbors()
     calcPressure()
     calcForce()
-    for (var i = 0; i < numParticles; i++) {
-      var p = particles[i]
+    for (let i = 0; i < numParticles; i++) {
+      let p = particles[i]
       p.move()
     }
   }
 
   function updateGrids () {
-    var i
-    var j
-    for (i = 0; i < NUM_GRIDS; i++) {
-      for (j = 0; j < NUM_GRIDS; j++) { grids[i][j].clear() }
+    for (let i = 0; i < NUM_GRIDS; i++) {
+      for (let j = 0; j < NUM_GRIDS; j++) { grids[i][j].clear() }
     }
-    for (i = 0; i < numParticles; i++) {
+    for (let i = 0; i < numParticles; i++) {
       var p = particles[i]
       p.fx = p.fy = p.density = 0
       p.gx = Math.floor(p.x * INV_GRID_SIZE)
@@ -172,12 +181,12 @@ window.onload = (function () {
 
   function findNeighbors () {
     numNeighbors = 0
-    for (var i = 0; i < numParticles; i++) {
-      var p = particles[i]
-      var xMin = p.gx !== 0
-      var xMax = p.gx !== NUM_GRIDS - 1
-      var yMin = p.gy !== 0
-      var yMax = p.gy !== NUM_GRIDS - 1
+    for (let i = 0; i < numParticles; i++) {
+      const p = particles[i]
+      const xMin = p.gx !== 0
+      const xMax = p.gx !== NUM_GRIDS - 1
+      const yMin = p.gy !== 0
+      const yMax = p.gy !== NUM_GRIDS - 1
       findNeighborsInGrid(p, grids[p.gx][p.gy])
       if (xMin) findNeighborsInGrid(p, grids[p.gx - 1][p.gy])
       if (xMax) findNeighborsInGrid(p, grids[p.gx + 1][p.gy])
@@ -191,10 +200,10 @@ window.onload = (function () {
   }
 
   function findNeighborsInGrid (pi, g) {
-    for (var j = 0; j < g.numParticles; j++) {
-      var pj = g.particles[j]
+    for (let j = 0; j < g.numParticles; j++) {
+      const pj = g.particles[j]
       if (pi === pj) { continue }
-      var distance = (pi.x - pj.x) * (pi.x - pj.x) + (pi.y - pj.y) * (pi.y - pj.y)
+      const distance = (pi.x - pj.x) * (pi.x - pj.x) + (pi.y - pj.y) * (pi.y - pj.y)
       if (distance < RANGE2) {
         if (neighbors.length === numNeighbors) { neighbors[numNeighbors] = new Neighbor() }
         neighbors[numNeighbors++].setParticle(pi, pj)
@@ -203,23 +212,23 @@ window.onload = (function () {
   }
 
   function calcPressure () {
-    for (var i = 0; i < numParticles; i++) {
-      var p = particles[i]
+    for (let i = 0; i < numParticles; i++) {
+      const p = particles[i]
       if (p.density < DENSITY) { p.density = DENSITY }
       p.pressure = p.density - DENSITY
     }
   }
 
   function calcForce () {
-    for (var i = 0; i < numNeighbors; i++) {
-      var n = neighbors[i]
+    for (let i = 0; i < numNeighbors; i++) {
+      const n = neighbors[i]
       n.calcForce()
     }
   }
   return function () {
-    for (var i = 0; i < NUM_GRIDS; i++) {
+    for (let i = 0; i < NUM_GRIDS; i++) {
       grids[i] = new Array(NUM_GRIDS)
-      for (var j = 0; j < NUM_GRIDS; j++) { grids[i][j] = new Grid() }
+      for (let j = 0; j < NUM_GRIDS; j++) { grids[i][j] = new Grid() }
     }
     window.addEventListener('mousedown', function (e) { mouseX = e.layerX; mouseY = e.layerY; press = true }, false)
     window.addEventListener('mousemove', function (e) { mouseX = e.layerX; mouseY = e.layerY }, false)
